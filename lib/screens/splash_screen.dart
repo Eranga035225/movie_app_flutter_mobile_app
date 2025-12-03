3d5b2cf3488713d953b77a6021580af82ce5240a
Eranga <erangakavishanka@icloud.com>
2025-12-03 23:55:30 +0530

    developed splash screen

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -10,6 +10,18 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(Icons.movie,size: 50,color: Colors.red,),
+            SizedBox(width: 10,),
+            Text("CinemateX",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 30),),
+          ],
+        ),
+      ),
+
+    );
   }
 }
