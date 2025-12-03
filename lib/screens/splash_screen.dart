40b960e40fb494a24ea29bef8678f3e1494ae1da
Eranga <erangakavishanka@icloud.com>
2025-12-04 00:13:41 +0530

    created screens

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,4 +1,8 @@
+import 'dart:async' show Timer;
+
 import 'package:flutter/material.dart';
+
+import 'home_screen.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -8,18 +12,31 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+
+  void initState(){
+
+    Timer(Duration(seconds: 3), () {
+      Navigator.pushReplacement((context), MaterialPageRoute(builder: (context) => HomeScreen()));
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Icon(Icons.movie,size: 50,color: Colors.red,),
-            SizedBox(width: 10,),
-            Text("CinemateX",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 30),),
-          ],
-        ),
+      backgroundColor: Colors.white,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(Icons.movie,size: 60,color: Colors.red,),
+              SizedBox(width: 10,),
+              Text("CinemateX",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 35),),
+            ],
+          ),
+          Text('Your companion to the world of cinema',style: TextStyle(color: Colors.grey,fontSize: 16),),
+         
+        ],
       ),
 
     );
