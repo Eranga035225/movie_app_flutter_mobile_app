a31aa506353a5262c58490be6deccf1f93359ebe
Eranga <erangakavishanka@icloud.com>
2025-12-05 00:20:28 +0530

    developed home screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -18,130 +18,168 @@ class _HomeScreenState extends State<HomeScreen> {
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 15.0,vertical: 8.0),
-          child: Column(children: [
-            Row(children: [
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text('Welcome Era👋', style: TextStyle(color: Colors.black,fontSize: 30,fontWeight: FontWeight.bold),),
-                  Text('Book your favourite movie', style: TextStyle(color: Colors.grey.shade600,fontSize: 17),),
-                ],
-              ),
-              Spacer(),
-              CircleAvatar(
-                radius: 20,
-                backgroundImage: NetworkImage('https://th.bing.com/th/id/OIP.w2McZSq-EYWxh02iSvC3xwHaHa?w=182&h=182&c=7&r=0&o=7&cb=ucfimg2&dpr=1.3&pid=1.7&rm=3&ucfimg=1'),
-                
-                )
-            ],),
-            SizedBox(height: 20,),
-            Row(children: [
-              Container(
-                width: size.width  * 0.77,
-                height: 50,
-                decoration: BoxDecoration(
-                  color: Colors.grey.shade100,
-                  borderRadius: BorderRadius.circular(10)
+          child: SingleChildScrollView(
+            physics: BouncingScrollPhysics(),
+            child: Column(children: [
+              Row(children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('Welcome Era👋', style: TextStyle(color: Colors.black,fontSize: 30,fontWeight: FontWeight.bold),),
+                    Text('Book your favourite movie', style: TextStyle(color: Colors.grey.shade600,fontSize: 17),),
+                  ],
                 ),
-                child: Center(
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: TextField(
-                      decoration: InputDecoration(
-                        border: InputBorder.none,
-                        prefixIcon: Icon(Icons.search,color: Colors.grey.shade600,size: 36),
-                        hintText: ' Search',
-                        hintStyle: TextStyle(color: Colors.grey.shade600,fontSize: 18),
+                Spacer(),
+                CircleAvatar(
+                  radius: 20,
+                  backgroundImage: NetworkImage('https://th.bing.com/th/id/OIP.w2McZSq-EYWxh02iSvC3xwHaHa?w=182&h=182&c=7&r=0&o=7&cb=ucfimg2&dpr=1.3&pid=1.7&rm=3&ucfimg=1'),
+                  
+                  )
+              ],),
+              SizedBox(height: 20,),
+              Row(children: [
+                Container(
+                  width: size.width  * 0.77,
+                  height: 50,
+                  decoration: BoxDecoration(
+                    color: Colors.grey.shade100,
+                    borderRadius: BorderRadius.circular(10)
+                  ),
+                  child: Center(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: TextField(
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          prefixIcon: Icon(Icons.search,color: Colors.grey.shade600,size: 36),
+                          hintText: ' Search',
+                          hintStyle: TextStyle(color: Colors.grey.shade600,fontSize: 18),
+                        ),
                       ),
                     ),
                   ),
-                ),
-                
-
-              ),
-              Spacer(),
-              Container(
-                width: 48,
-                height: 50,
-                decoration: BoxDecoration(
-                  color: Colors.red.shade800,
-                  borderRadius: BorderRadius.circular(10)
-                ),
-                child: Icon(Icons.tune_rounded,color: Colors.white,),
-              )
-
-            ],),
-            SizedBox(height: 20),
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-              Text('Category', style: TextStyle(color: Colors.black,fontSize: 18,fontWeight: FontWeight.bold),),
-              Spacer(),
-              Text('See All', style: TextStyle(color: Colors.red.shade800,fontSize: 15),),
-            ],),
-            SizedBox(height: 20,),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Category(emoji: '🥰', type: 'Romance'),
-                Category(emoji: '😂', type: 'Comedy'),
-                Category(emoji: '😱', type: 'Horror'),
-                Category(emoji: '😁', type: 'Drama'),
-
-                 
-
-              ],
-            ),
-            SizedBox(height: 25,),
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-              Text('Latest Movie', style: TextStyle(color: Colors.black,fontSize: 18,fontWeight: FontWeight.bold),),
-              Spacer(),
-              Text('See All', style: TextStyle(color: Colors.red.shade800,fontSize: 15),),
-            ],),
-
-            SizedBox(height: 10,),
-            SizedBox(
-              height: 250,
-              child: ListView.builder(
-                scrollDirection: Axis.horizontal,
-                itemCount: films.length,
-                
-                itemBuilder: (context, index) {
-                  var film = films[index];
-                  return Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Container(
-                      width: 150,
-                      
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        image: DecorationImage(image: NetworkImage(film.imageUrl),fit: BoxFit.cover)
-                      ),
-                              
-                              
-                              
-                              
-                    ),
-                  );
-                }),
-            )
-
-
-
-
+                  
             
+                ),
+                Spacer(),
+                Container(
+                  width: 48,
+                  height: 50,
+                  decoration: BoxDecoration(
+                    color: Colors.red.shade800,
+                    borderRadius: BorderRadius.circular(10)
+                  ),
+                  child: Icon(Icons.tune_rounded,color: Colors.white,),
+                )
+            
+              ],),
+              SizedBox(height: 20),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                Text('Category', style: TextStyle(color: Colors.black,fontSize: 18,fontWeight: FontWeight.bold),),
+                Spacer(),
+                Text('See All', style: TextStyle(color: Colors.red.shade800,fontSize: 15),),
+              ],),
+              SizedBox(height: 20,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Category(emoji: '🥰', type: 'Romance'),
+                  Category(emoji: '😂', type: 'Comedy'),
+                  Category(emoji: '😱', type: 'Horror'),
+                  Category(emoji: '😁', type: 'Drama'),
+            
+                   
+            
+                ],
+              ),
+              SizedBox(height: 25,),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                Text('Latest Movies', style: TextStyle(color: Colors.black,fontSize: 18,fontWeight: FontWeight.bold),),
+                Spacer(),
+                Text('See All', style: TextStyle(color: Colors.red.shade800,fontSize: 15),),
+              ],),
+            
+              SizedBox(height: 10,),
+              SizedBox(
+                height: 250,
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: films.length,
+                  
+                  itemBuilder: (context, index) {
+                    var film = films[index];
+                    return Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Container(
+                        width: 150,
+                        
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          image: DecorationImage(image: NetworkImage(film.imageUrl),fit: BoxFit.cover)
+                        ),
+                                
+                                
+                                
+                                
+                      ),
+                    );
+                  }),
+              ),
+              SizedBox(height: 20),
+               Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                Text('Favourite Movies', style: TextStyle(color: Colors.black,fontSize: 18,fontWeight: FontWeight.bold),),
+                Spacer(),
+                Text('See All', style: TextStyle(color: Colors.red.shade800,fontSize: 15),),
+              ],),
+            
+              SizedBox(
+                height: 250,
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: films.length,
+                  
+                  itemBuilder: (context, index) {
 
-
-          
-          
-          
-          
-          
-          
-          
-          ],),
+                    var film = films[index+1];
+                    return Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Container(
+                        width: 150,
+                        
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          image: DecorationImage(image: NetworkImage(film.imageUrl),fit: BoxFit.cover)
+                        ),
+                                
+                                
+                                
+                                
+                      ),
+                    );
+                  }),
+              ),
+            
+            
+            
+            
+              
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            ],),
+          ),
         ),
       )
 
