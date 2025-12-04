ebcd98e9f8f94c8334138b68c3136ba043924b05
Eranga <erangakavishanka@icloud.com>
2025-12-04 10:28:37 +0530

    dveeloepd section 2

--- lib/screens/onboard_screen1.dart
+++ lib/screens/onboard_screen1.dart
@@ -16,18 +16,28 @@ class _OnBoardScreen1State extends State<OnBoardScreen1> {
         child: Padding(
           padding: const EdgeInsets.all(8.0),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             
             children: [
               Text.rich(
                 TextSpan(
                   text: 'Find the',
-                  style: TextStyle(color: Colors.black,fontSize: 30,fontWeight: FontWeight.bold),
+                  style: TextStyle(color: Colors.black,fontSize: 35,fontWeight: FontWeight.bold),
                   children: [
-                    TextSpan(text: ' latest and\ngreatest',style: TextStyle(color: Colors.red,fontSize: 30,fontWeight: FontWeight.bold)),
-                    TextSpan(text: ' movie here',style: TextStyle(color: Colors.black,fontSize: 30,fontWeight: FontWeight.bold)),
+                    TextSpan(text: ' latest and\ngreatest',style: TextStyle(color: Colors.red,fontSize: 35,fontWeight: FontWeight.bold)),
+                    TextSpan(text: ' movie here',style: TextStyle(color: Colors.black,fontSize: 35,fontWeight: FontWeight.bold)),
                   ]
           
-                ))]
+                )),
+                SizedBox(height: 10,),
+                Text('Discover top-rated movies, trending hits,\nand new releases all in one place..',style: TextStyle(color: Colors.grey.shade700,fontSize: 16),),
+                SizedBox(height: 8),
+                Row(children: [
+                  
+                ],)
+
+                
+                ]
                   
                   
                   
