0a01b74c973ced85dc75a54f159ef7e58346924e
Eranga <erangakavishanka@icloud.com>
2025-12-04 12:03:18 +0530

    changed styles

---
+++ lib/screens/onboard_screen3.dart
@@ -0,0 +1,115 @@
+import 'package:flutter/material.dart';
+
+import 'login_screen.dart';
+
+class OnBoardScreen3 extends StatefulWidget {
+  const OnBoardScreen3({super.key});
+
+  @override
+  State<OnBoardScreen3> createState() => _OnBoardScreen3State();
+}
+
+class _OnBoardScreen3State extends State<OnBoardScreen3> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey.shade100,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.only(top:38.0,left: 12,right: 12),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            
+            children: [
+              Text.rich(
+                TextSpan(
+                  text: 'Book your',
+                  style: TextStyle(color: Colors.black,fontSize: 35,fontWeight: FontWeight.bold),
+                  children: [
+                    TextSpan(text: ' favourite\nmovie',style: TextStyle( color: Colors.red.shade800,fontSize: 35,fontWeight: FontWeight.bold)),
+                    TextSpan(text: ' tickets today',style: TextStyle(color: Colors.black,fontSize: 35,fontWeight: FontWeight.bold)),
+                  ]
+          
+                )),
+
+
+              SizedBox(height: 10,),
+              Text('Find what to watch next with\nsmart suggestions and curated lists...',style: TextStyle(color: Colors.grey.shade600,fontSize: 16),),
+              SizedBox(height: 10),
+              Row(children: [
+                  Container(
+                    width: 80,
+                    height: 8,
+                    decoration: BoxDecoration(
+                      color:  Colors.red.shade800,
+                      borderRadius: BorderRadius.circular(10)
+
+                    ),
+                  ),
+                  SizedBox(width: 8),
+                  CircleAvatar(
+                    radius: 4,
+                    backgroundColor: Colors.grey.shade300
+                  ),
+                   SizedBox(width: 8),
+                  CircleAvatar(
+                    radius: 4,
+                    backgroundColor: Colors.grey.shade300
+                  )
+                  
+
+                ],
+                
+              ),
+              SizedBox(height: 15),
+              IconButton(
+                onPressed: (){
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => LoginScreen()));  
+                  
+
+                } 
+              , icon: Icon(Icons.arrow_forward_ios_rounded),
+              color: Colors.white,
+                style: ButtonStyle(
+                  backgroundColor: WidgetStatePropertyAll(
+                    Colors.red.shade800
+                  )
+                )
+
+
+              ),
+
+              SizedBox(height: 40),
+             
+              Image.asset('assets/images/girl3.png'),
+
+
+
+
+
+              
+              
+              
+              
+            
+
+                
+                ]
+                  
+                  
+                  
+                  ),
+        ))
+              
+          
+
+
+
+
+
+
+
+
+    );
+  }
+}
