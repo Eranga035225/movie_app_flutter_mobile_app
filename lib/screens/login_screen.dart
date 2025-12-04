d46dd35a7a4c427839f07efb7a83b95dfc8d63af
Eranga <erangakavishanka@icloud.com>
2025-12-04 12:17:09 +0530

    developed container for login fields

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -6,7 +6,31 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey.shade100,
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Center(
+            child: Column(children: [
+              Image.asset('assets/images/logo2.png',width: 200,),
+              SizedBox(height: 15,),
+              Text('Welcome Back',style: TextStyle(color: Colors.black,fontSize: 25,fontWeight: FontWeight.bold),),
+              Text('Log in to your account using email or social networls',style: TextStyle(color: Colors.grey.shade600,fontSize: 15),textAlign: TextAlign.center,),
+              SizedBox(height: 30,),
+              Container(
+                width: 350,
+                height: 55,
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.grey.shade400,),
+                    borderRadius: BorderRadius.circular(10),
+                )),
+              
+              
+            ],),
+          ),
+        ),
+      )
       
 
 
