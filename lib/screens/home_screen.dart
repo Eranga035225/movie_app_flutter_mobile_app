65bebda04b4101c1e7a7bd32a36c6e81004d5f47
Eranga <erangakavishanka@icloud.com>
2025-12-04 22:34:25 +0530

    developed second row

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -10,6 +10,7 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
+    final size  = MediaQuery.of(context).size;
     return Scaffold(
       backgroundColor: Colors.white,
       body: SafeArea(
@@ -30,6 +31,43 @@ class _HomeScreenState extends State<HomeScreen> {
                 backgroundImage: NetworkImage('https://th.bing.com/th/id/OIP.w2McZSq-EYWxh02iSvC3xwHaHa?w=182&h=182&c=7&r=0&o=7&cb=ucfimg2&dpr=1.3&pid=1.7&rm=3&ucfimg=1'),
                 
                 )
+            ],),
+            SizedBox(height: 20,),
+            Row(children: [
+              Container(
+                width: size.width  * 0.77,
+                height: 50,
+                decoration: BoxDecoration(
+                  color: Colors.grey.shade100,
+                  borderRadius: BorderRadius.circular(10)
+                ),
+                child: Center(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        prefixIcon: Icon(Icons.search,color: Colors.grey.shade600,size: 36),
+                        hintText: ' Search',
+                        hintStyle: TextStyle(color: Colors.grey.shade600,fontSize: 18),
+                      ),
+                    ),
+                  ),
+                ),
+                
+
+              ),
+              Spacer(),
+              Container(
+                width: 48,
+                height: 50,
+                decoration: BoxDecoration(
+                  color: Colors.red.shade800,
+                  borderRadius: BorderRadius.circular(10)
+                ),
+                child: Icon(Icons.tune_rounded,color: Colors.white,),
+              )
+
             ],)
           
           
